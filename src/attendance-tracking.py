5c17f9fbc3a24482870fd3c7d07e6deff2ec26e5
Johannes Rohwer <mail@johannesrohwer.com>
2017-12-18 15:09:41 +0100

    add statemanager

---
+++ src/attendance-tracking.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+print('Welcome to ASE Attendance Tracking')
+
+class State:
+
+    def execute(self):
+        raise NotImplementedError
+
+    def handle_input(self, event):
+        raise NotImplementedError
+
+    def transition(self):
+        raise NotImplementedError
+    
+class StateManager:
+    states = []
+
+    def push(self, state):
+        self.push(self.states)
+        self.peek().execute()
+
+    def pop(self):
+        return self.states.pop()
+
+    def replace(self, state):
+        self.pop()
+        self.push(state)
+
+    def peek(self):
+        return self.states[-1]
+
+    def reset(self):
+        states = [] # TODO: set idle state
